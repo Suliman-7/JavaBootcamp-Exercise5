baade52fc1a601d0f4afc9f323b4116bbfa8352a
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-07-09 16:52:32 +0300

    Exercise5 edited

---
+++ Exercise5.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+
+public class Exercise5 {
+
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+
+//                System.out.println("Please enter first number ");
+//                int firstNum = input.nextInt();
+//                System.out.println("Please enter second number ");
+//                int secondNum = input.nextInt();
+//                System.out.println("Please enter third number ");
+//                int thirdNum = input.nextInt();
+//
+//
+//                smallest(firstNum ,secondNum , thirdNum) ;
+//
+//              System.out.println("Please enter a number ");
+//              int num = input.nextInt();
+//
+//              check(num);
+//		System.out.println("Please enter your password (You are agreeing to the above Terms and Conditions.):");
+//		String password = input.next();
+//
+//
+//		System.out.println(checkPassword(password));
+//	}
+		//            public static void smallest (int num1 , int num2 , int num3) {
+//
+//            	if ( num1 < num2 && num1 < num3 ) {
+//                  System.out.println( "The Smallest Value :" +  num1 );
+//            	}
+//            	else if ( num2 < num1 && num2 < num3) {
+//                  System.out.println( "The Smallest Value :" +  num2 );
+//            	}
+//            	else if( num3 < num1 && num3 < num1 ) {
+//                  System.out.println( "The Smallest Value :" +  num3 );
+//            	}else {
+//                  System.out.println( " All numbers are equal " );
+//            	}
+//
+//            }
+//
+//          public static void check (int num) {
+//
+//        	if ( num > 0 ) {
+//              System.out.println( "Positive" );
+//        	}
+//        	else if ( num < 0 ) {
+//              System.out.println( "Negative" );
+//        	}else {
+//              System.out.println( " Zero " );
+//        	}
+//
+//        }
+//
+//
+//	public static String checkPassword (String password) {
+//
+//		boolean hasLtr = false ;
+//
+//		int numCounter = 0 ;
+//
+//
+//		if(password.length()< 8) {
+//			return "Password should be at least eight digits!" ;
+//		}
+//
+//		for ( int i=0 ; i<password.length() ; i++ ) {
+//
+//			char ch = password.charAt(i) ;
+//
+//			if (Character.getType(ch) == 2 || Character.getType(ch) == 1 ) {
+//				hasLtr = true  ;
+//			}
+//
+//
+//			if (Character.getType(ch) != 2 && Character.getType(ch) != 1 && Character.getType(ch) != 9) {
+//				return "Password should contains only letters and digits!" ;
+//			}
+//
+//			if (Character.getType(ch)==9) {
+//				numCounter++ ;
+//			}
+//		}
+//
+//		if (numCounter<2) {
+//			return "Password should contains at least 2 digits!" ;
+//		}
+//
+//		if (hasLtr==false){
+//			return "Password should contain letters !" ;
+//		}
+//		else {
+//			return "Password is valid : " + password;
+//		}
+//	}
+
+
+	}}
